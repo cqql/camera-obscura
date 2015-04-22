f3476371d7c3cd2a0c193cce9300daff3064e624
Marten Lienen <marten.lienen@gmail.com>
2015-04-22 13:29:33 +0200

    Add skeleton for android app

---
+++ android/src/de/cqql/camera_obscura/android/RecordingActivity.java
@@ -0,0 +1,13 @@
+package de.cqql.camera_obscura.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordingActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+}
